da8dd274aa7462e077594ffe971b15fb18993377
Martin Kvammen <martin.kvammen@gmail.com>
2022-10-18 17:43:53 +0200

    new_notebook

---
+++ eda_lonnstall.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+
+
+df = pd.read_csv("https://raw.githubusercontent.com/HaliaeetusAlbicilla/k24salary/master/kode24salary.csv")
+
+df.head()
+
+
